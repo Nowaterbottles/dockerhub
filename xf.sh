21ac65bbc1afda560258cab8ce93d3267fa772fc
Nowaterbottles <61109993+Nowaterbottles@users.noreply.github.com>
2022-07-09 00:22:45 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "a17ccf7a-6290-4cd4-b6ac-eb9085268286",
                         "alterId": 0
                     }
                 ],
